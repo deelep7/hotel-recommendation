e734acb5be415d7f2290b6506ece3f0c5fc68c2b
deelep7 <dilip7poudel@gmail.com>
2025-05-02 16:15:53 +0545

    Commited at master

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,43 +9,82 @@ import Layout from "./layouts/Layout";
 import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
-
-
+import MyHotels from "./pages/MyHotels";
+import EditHotel from "./pages/EditHotel";
 
 const App = () => {
-  const {isLoggedIn} = useAppContext();
-  return(
+  const { isLoggedIn } = useAppContext();
+
+  return (
     <Router>
       <Routes>
-        <Route path="/" element ={<Layout>
-          <p>Home Page</p>
-        </Layout>
-      } />
-        <Route path="/search" element ={
-          <Layout>
-          <p>Search Page</p>
-        </Layout>
-        } />
-        <Route path = "/register" element={<Layout>
-          <Register />
-        </Layout>} />
-          <Route path="/sign-in" element={
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p>Search Page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
+        <Route
+          path="/sign-in"
+          element={
             <Layout>
               <SignIn />
             </Layout>
-          } />
-        {isLoggedIn && <>
-        <Route path="/add-hotel" element={
-          <Layout>
-            <AddHotel />
-          </Layout>
-        } >
-          </Route></>}
-        <Route path="*" element ={<Navigate to="/" />} />
+          }
+        />
+
+{isLoggedIn && (
+  <>
+    <Route
+      path="/add-hotel"
+      element={
+        <Layout>
+          <AddHotel />
+        </Layout>
+      }
+    />
+    <Route
+      path="/my-hotels"
+      element={
+        <Layout>
+          <MyHotels />
+        </Layout>
+      }
+    />
+      <Route
+      path="/edit-hotel/:hotelId"
+      element={
+        <Layout>
+          <EditHotel />
+        </Layout>
+      }
+    />
+  </>
+)}
+
+
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
 };
-
 
 export default App;
